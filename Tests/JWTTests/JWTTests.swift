e73f74e712418494319328c739fcacdae5b6f1a5
Sergey Minakov <s.minakov@be-interactive.ru>
2017-01-10 11:39:34 +0300

    tests

--- Tests/JWTTests/JWTTests.swift
+++ Tests/JWTTests/JWTTests.swift
@@ -1,11 +1,22 @@
 import XCTest
+import Foundation
 @testable import JWT
 
 class JWTTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(JWT().text, "Hello, World!")
+        var string = "Hello1"
+        var encoded = try? Encoding.base64.encode(string: string)
+        var decoded = try? Encoding.base64.decode(string: encoded!)
+        
+        print("\(string), \(encoded), \(decoded), \(String(data: decoded!, encoding: .utf8))")
+        
+        string = "Hello2"
+        encoded = try? Encoding.base64URL.encode(string: string)
+        decoded = try? Encoding.base64URL.decode(string: encoded!)
+        
+        print("\(string), \(encoded), \(decoded), \(String(data: decoded!, encoding: .utf8))")
     }
 
 
